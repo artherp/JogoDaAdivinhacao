e8a788d7a8940749dca7c9568447c62d98cb5347
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-07 18:44:12 -0300

    Refatorando e algumas funcionalidades extras

--- main.js
+++ main.js
@@ -1,18 +1,52 @@
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
+//variaveis
+const screen1 = document.querySelector('.screen1');
+const screen2 = document.querySelector('.screen2');
+const btnTry = document.querySelector('#btnTry');
+const btnReset = document.querySelector('#btnReset');
 
-function handleClick(event) {
-    event.preventDefault()
+let randomNumber = Math.round(Math.random() * 10);
 
-    const inputNumber = document.querySelector("#inputNumber")
+let xAttempts = 1;
 
-    if(Number(inputNumber.value) == randomNumber) {
-        document.querySelector('.screen1').classList.add('hide')
-        document.querySelector('.screen2').classList.remove('hide')
-        document.querySelector(
-            '.screen2 h2'
-          ).innerText = `acertou em ${xAttempts} tentativas`
+// eventos
+btnTry.addEventListener('click', handleTryClick);
+btnReset.addEventListener('click', handleResetClick);
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick
     }
+})
 
-    xAttempts++
+//funções
+function handleTryClick(event) {
+  event.preventDefault();
+
+  const inputNumber = document.querySelector('#inputNumber');
+
+  if (inputNumber.value === '' || Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10) {
+    alert('Por favor, insira um número válido!');
+    return;
+}
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+    document.querySelector(
+      '.screen2 h2'
+    ).innerText = `Acertou em ${xAttempts} tentativas`;
+  }
+
+  inputNumber.value = '';
+  xAttempts++;
+}
+
+function handleResetClick() {
+    toggleScreen()
+    xAttempts = 1;
+    randomNumber = Math.round(Math.random() * 10);
+    document.querySelector('#inputNumber').value = '';
+}
+
+function toggleScreen() {
+    screen1.classList.toggle('hide');
+    screen2.classList.toggle('hide');
 }
